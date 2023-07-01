262a61a08263e93e61aab670e3c8e82f50a2f796
MaviYazici <maviisikyazici@gmail.com>
2023-07-01 11:39:26 +0300

    Add project files.

---
+++ App.Web.Mvc/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace App.Web.Mvc.Areas.Admin.Controllers
+{
+	public class DashboardController : Controller
+	{
+
+        [Area("Admin")]
+        [Route("Admin/[controller]/[action]")]
+        public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
